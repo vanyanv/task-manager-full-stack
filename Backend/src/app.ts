ef849a9d21c4919dae85fc86b515d094f7073868
Vardan <vardan.vanyan@gmail.com>
2024-11-20 16:07:55 -0800

    added routes and models and connected mongo db

---
+++ Backend/src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import todoRoutes from './routes/todoRoutes';
+dotenv.config();
+
+//express
+const app = express();
+//cors
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGODB_URI || '')
+  .then(() => console.log('Connected To MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+export default app;
--- Backend/src/models/Todo.ts
+++ Backend/src/models/Todo.ts
@@ -9,14 +9,36 @@ export interface ITodo extends Document {
   updatedAt: Date;
 }
 
+export enum TodoCategories {
+  WORK = 'Work',
+  PERSONAL = 'Personal',
+  HOME = 'Home',
+  HEALTH = 'Health',
+  FINANCE = 'Finance',
+  SHOPPING = 'Shopping',
+  FAMILY = 'Family',
+  SOCIAL = 'Social',
+  LEARNING = 'Learning',
+  ERRANDS = 'Errands',
+  PRIORITY = 'Priority',
+  WEEKLY_GOALS = 'Weekly Goals',
+  PROJECTS = 'Projects',
+  CREATIVE = 'Creative',
+  ADMIN = 'Admin',
+  TRAVEL = 'Travel',
+  GOALS = 'Goals',
+  FOLLOW_UP = 'Follow-Up',
+  REVIEW = 'Review',
+  MISCELLANEOUS = 'Miscellaneous',
+}
+
 const TodoSchema: Schema = new Schema({
   title: { type: String, required: true },
   description: { type: String },
   completed: { type: Boolean, default: false },
-  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  categories: { type: TodoCategories },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model<ITodo>('Todo', TodoSchema);
-sdsd
